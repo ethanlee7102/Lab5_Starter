61859c543315a44a4bbcae2960aaadb7ec86e829
ethanlee7102 <109195042+ethanlee7102@users.noreply.github.com>
2024-05-07 04:49:19 -0700

    Update unit.test.js

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -14,6 +14,6 @@ test("valid phone numbers", () => {
 });
 
 test("invalid phone numbers", () => {
-  expect(isPhoneNumber("(123) 46786-7890")).toBe(false);
+  expect(isPhoneNumber("(123) 45-7890")).toBe(false);
   expect(isPhoneNumber("hi")).toBe(false);
 });
