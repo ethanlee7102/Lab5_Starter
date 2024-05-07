8c9f2f3f26245272d08469fa1418e0f5e3accbb9
ethanlee7102 <ethanlee7102@gmail.com>
2024-05-06 23:54:04 -0700

    change image

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,19 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectElement = document.getElementById("horn-select");
+
+  const allImages = {
+    "air-horn" : "assets/images/air-horn.svg",
+    "car-horn" : "assets/images/car-horn.svg",
+    "party-horn" : "assets/images/party-horn.svg"
+  };
+
+  const hornImage = document.querySelector("img[alt='No image selected']");
+
+  selectElement.addEventListener("change", function(e) {
+    if (hornImage[e.target.value]) {
+      hornImage.src = hornImage[e.target.value];
+    }
+  });
 }
